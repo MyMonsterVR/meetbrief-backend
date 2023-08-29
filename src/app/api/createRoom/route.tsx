c8944522cb2a82c975b1bb88a67229972dec9465
Marcus Carlsen <marcuscarlsen@hotmail.dk>
2023-08-29 13:07:44 +0200

    I think it work

--- src/app/api/createRoom/route.tsx
+++ src/app/api/createRoom/route.tsx
@@ -92,7 +92,7 @@ export async function POST(req: NextRequest, res: NextApiResponse) {
       try {
         let newRoom = null as any;
         await twilioClient.video.v1.rooms.create({uniqueName: roomNameInput, type: "go"}).then(room => newRoom = room);
-        return { msg: "Successful", status: "success", mes: `We dead ${newRoom.sid}`, newRoom};
+        return { msg: "Successful", status: "success", uniqueName: newRoom.uniqueName};
       } catch (error: any) {
         return { msg: "Successful", status: "success", room: "We alive", error};
       }
